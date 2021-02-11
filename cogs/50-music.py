3cd95a89804dba6a23c57f6c7280b77bdb672bcc
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2021-02-10 16:00:02 -0800

    Audio rework (50%)

--- cogs/50-music.py
+++ cogs/50-music.py
@@ -4,13 +4,21 @@ from discord.ext import commands
 import discord
 import youtube_dl
 import os
+import datetime
 from youtube_search import YoutubeSearch
 
 
 class Music(commands.Cog):
     def __init__(self, client):
         self.client = client
-        os.system('rm -rf /tmp/Yup_music/* &> /dev/null && mkdir /tmp/Yup_music &> /dev/null')
+        options = {
+                'format': 'bestaudio/best',
+                'noplaylist': True,
+                'ignoreerrors': False,
+                'quiet': True,
+                'no_warnings': True
+                }
+        self.ytdl = youtube_dl.YoutubeDL(options)
 
     @commands.command()
     async def join(self, ctx):
@@ -19,18 +27,16 @@ class Music(commands.Cog):
         :param ctx:
         :return:
         """
-        vs = ctx.author.voice
-        # print(vs)
-        vc = vs.channel
-        # print(vc)
-        # print(type(vc))
-        # print('yeet')
-        # a = discord.VoiceClient(channel=vc)
-        # await a.connect(reconnect=False, timeout=0.01)
-        # await vc.connect()
-        # I am so fcking tired right now lmao
-        await vc.connect()
-        # print('done?')
+        if ctx.author.voice is not None:
+            if ctx.voice_client is not None and ctx.voice_client.channel != ctx.author.voice.channel:
+                await ctx.voice_client.move_to(ctx.author.voice.channel)
+                ctx.reply('Connnected!')
+            else:
+                vc = ctx.author.voice.channel
+                await vc.connect()
+                await ctx.reply('Connected!')
+        else:
+            await ctx.reply('You are not connected to a voice channel.')
 
     @commands.command()
     async def leave(self, ctx):
@@ -38,43 +44,94 @@ class Music(commands.Cog):
         Disconnects bot
         :return:
         """
-        await ctx.guild.voice_client.disconnect()
+        if ctx.guild.voice_client is not None:
+            await ctx.guild.voice_client.disconnect()
+            await ctx.reply('Disconnected!')
+        else:
+            await ctx.reply('I am not connected to a voice channel')
 
-    @commands.command()
-    async def download(self, ctx, *urls):
+    async def embed_maker(self, ctx, ytdl_info):
         """
-        Downloads a song from URL
-        :param ctx:
-        :param url:
-        :return:
+        Embed maker for play command.
         """
-        await ctx.reply(embed=discord.Embed(title='Downloading...', color=0xFEFFFF))
-        os.system('mkdir /tmp/Yup_music &> /dev/null')
-        with youtube_dl.YoutubeDL({'outtmpl': '/tmp/Yup_music/%(title)s.%(ext)s'}) as ydl:
-            ydl.download(list(urls))
-        await ctx.reply(embed=discord.Embed(title='Downloaded!', color=0xFEFFFF, description='\n'.join(list(urls))))
-    
-    @commands.command(name='listmusic', aliases=['lm'])
-    async def listmusic(self, ctx):
-        a = os.listdir('/tmp/Yup_music/')
-        embed = discord.Embed(title='Availible Music', color=0xFEFFFF, description='\n'.join(a))
+        embed = discord.Embed(title='Playing', color=0xFEFFFF, )
+        try:
+            embed.set_thumbnail(url=ytdl_info['thumbnail'])
+        except:
+            pass
+        try:
+            embed.add_field(name='Title', value=f'[{ytdl_info["title"]}]({ytdl_info["url"]})', inline=False)
+        except:
+            pass
+        try:
+            embed.add_field(name='Channel', value=f'[{ytdl_info["uploader"]}]({ytdl_info["uploader_url"]})', inline=False)
+        except:
+            pass
+        try:
+            embed.add_field(name='Duration', value=f'{str(datetime.timedelta(seconds=ytdl_info["duration"]))}', inline=False)
+        except:
+            pass
         await ctx.reply(embed=embed)
 
-    @commands.command()
     async def play(self, ctx, name):
-        b = ctx.guild.voice_client
-        b.play(discord.FFmpegOpusAudio(name))
+        if ctx.guild.voice_client is not None and ctx.author.voice is not None:
+            if ctx.author.voice.channel == ctx.guild.voice_client.channel:
+                b = ctx.guild.voice_client
+                c = self.ytdl.extract_info(url=name, download=False)
+                await self.embed_maker(ctx=ctx, ytdl_info=c)
+                b.play(discord.FFmpegOpusAudio(c['url']))
+            else:
+                if ctx.guild.voice_client.is_playing() or ctx.guild.voice_client.is_paused():
+                    await ctx.guild.voice_client.stop()
+                await ctx.guild.voice_client.move_to(ctx.author.voice.channel)
+                b = ctx.guild.voice_client
+                c = self.ytdl.extract_info(url=name, download=False)
+                await self.embed_maker(ctx=ctx, ytdl_info=c)
+                b.play(discord.FFmpegOpusAudio(c['url']))
+        elif ctx.author.voice is not None:
+            b = await ctx.author.voice.channel.connect()
+            await self.embed_maker(ctx=ctx, ytdl_info=self.ytdl.extract_info(url=name, download=False))
+            b.play(discord.FFmpegOpusAudio(self.ytdl.extract_info(url=name, download=False)['url']))
+        else:
+            await ctx.reply('You are not connected to a voice channel')
 
+    @commands.command(name='play')
+    async def _play(self, ctx, name):
+        await play(ctx=ctx, name=name)
+
+    @_play.error
+    async def play_error(self, ctx, error):
+        print(f'\e[0;31m---------------\nctx: {ctx}\nError: {error}\n---------------\e[0m')
+        
     @commands.command()
     async def search(self, ctx, *terms):
         """
         """
+        import asyncio
         term = ' '.join(terms)
         results = YoutubeSearch(term, max_results=10).to_dict()
         embed = discord.Embed(title='Results', color=0xFEFFFF)
         for i in results:
             embed.add_field(name=i["title"], value=f'Channel: {i["channel"]}\nURL: https://youtube.com{i["url_suffix"]}\nDuration: {i["duration"]}\nViews: {i["views"]}\n[Thumbnail]({i["thumbnails"][0]})', inline=False)
         await ctx.reply(embed=embed)
+        channel = ctx.channel
+
+        def check_int(e):
+            try:
+                int(e.content)
+                if e.channel == channel:
+                    return True
+                else:
+                    return False
+            except:
+               return False
+        try:
+            num = await self.client.wait_for('message', timeout=60.0, check=check_int)
+        except Exception as e:
+            print(e)
+            await ctx.reply('You did not choose a valid song in time.')
+        else:
+            await self.play(ctx=ctx, name=f'https://youtube.com{results[int(num.content) - 1]["url_suffix"]}')
 
     @search.error
     async def yeet(self, ctx, error):
